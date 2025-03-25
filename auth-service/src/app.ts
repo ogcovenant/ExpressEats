f150fcf4750919580d5af0802dc062e515f45784
Covenant Ogowale <justcovenant@outlook.com>
2025-03-25 10:50:36 +0100

    added db connection and basic middleware config

--- auth-service/src/app.ts
+++ auth-service/src/app.ts
@@ -1,8 +1,24 @@
-import express from 'express';
+import express, { NextFunction, Request, Response } from "express";
+import helmet from "helmet";
+import cors from "cors";
+import logger from "./utils/logger";
+import Redis from "ioredis";
+import { RateLimiterRedis } from "rate-limiter-flexible";
 
 const app = express();
 
+app.use(helmet());
+app.use(cors());
 app.use(express.json());
+
+app.use((req: Request, res: Response, next: NextFunction) => {
+  logger.info(`Received ${req.method} request to ${req.url}`);
+  logger.info(`Request body, ${req.body}`);
+  next();
+});
+
+const redisClient = new Redis(process.env.REDIS_URL as string); 
+
 
 
 export default app;
--- auth-service/src/server.ts
+++ auth-service/src/server.ts
@@ -1,7 +1,21 @@
 import app from "./app";
+import logger from "./utils/logger";
+import dotenv from "dotenv";
+import mongoose from "mongoose";
+
+dotenv.config();
 
 const PORT = process.env.PORT || 3000;
 
+mongoose
+  .connect(process.env.MONGODB_URI as string)
+  .then(() => {
+    logger.info("Connected to MongoDB...");
+  })
+  .catch((error) => {
+    logger.error("MongoDB connection error: ", error);
+  });
+
 app.listen(PORT, () => {
-  console.log(`Server is running on port ${PORT}`);
+  logger.info(`Server is running on port ${PORT}`);
 });

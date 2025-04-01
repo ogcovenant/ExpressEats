b85cf1793e5b9a064c24a7059d54073882d5e13c
Covenant Ogowale <justcovenant@outlook.com>
2025-04-02 00:31:10 +0100

    added interface declarations for the validation helpers

--- auth-service/src/helpers/validation.helper.ts
+++ auth-service/src/helpers/validation.helper.ts
@@ -1,6 +1,11 @@
 import Joi from "joi";
+import {
+  ILoginData,
+  IRefreshTokenData,
+  IRegisterData,
+} from "../interfaces/validation-request.interfaces";
 
-export const validateRegistration = (data: any) => {
+export const validateRegistration = (data: IRegisterData) => {
   const schema = Joi.object({
     username: Joi.string().min(3).max(50).required(),
     email: Joi.string().email().required(),
@@ -10,10 +15,18 @@ export const validateRegistration = (data: any) => {
   return schema.validate(data);
 };
 
-export const validateLogin = (data: any) => {
+export const validateLogin = (data: ILoginData) => {
   const schema = Joi.object({
     email: Joi.string().email().required(),
     password: Joi.string().min(6).required(),
+  });
+
+  return schema.validate(data);
+};
+
+export const validateRefreshToken = (data: IRefreshTokenData) => {
+  const schema = Joi.object({
+    refreshToken: Joi.string().email().required(),
   });
 
   return schema.validate(data);
---
+++ auth-service/src/interfaces/validation-request.interfaces.ts
@@ -0,0 +1,14 @@
+export interface IRegisterData {
+  username: string;
+  email: string;
+  password: string;
+}
+
+export interface ILoginData {
+  email: string;
+  password: string;
+}
+
+export interface IRefreshTokenData {
+  refreshToken: string;
+}
